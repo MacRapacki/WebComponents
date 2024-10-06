7fd7a64d08097370be7c6f1894ee4a01de56c6f2
Maciej Rapacki <mac.rapacki@gmail.com>
2024-10-06 16:28:31 +0200

    chore: code cleanup

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
 import { NotesDataService } from './services/notesService.js';
 
-const data = [1, 2, 3, 4, 5];
-const updateData = (newData) => {
-    data = newData;
-};
-
-const searchBar = document.querySelector('search-bar');
-const asd = document.querySelector('.noNotesWrapper');
-searchBar.setAttribute('data', data);
-searchBar.setAttribute('handle-data', updateData);
-
 NotesDataService.initializeState([]);
