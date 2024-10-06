0a1cc20ef4143169b7c456494dfe83dfcf606225
Maciej Rapacki <mac.rapacki@gmail.com>
2024-10-06 15:56:24 +0200

    feat: add search bar style

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,15 +1,27 @@
 const template = document.createElement('template');
 template.innerHTML = `
-        <style>
-        input {
-            width: 100%;
-            height: 40px;
-            border: none;
-            background-color: #EEEFF0;
-            padding: 8px 12px;
-            border-radius: 12px;
-            box-sizing: border-box;
-        }
+<style>
+
+    input[type='text'] {
+    background-color: #eeeff0;
+    color: #5b5c5e;
+    padding: 8px 12px;
+    border: none;
+    border-radius: 12px;
+    font-size: 16px;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+input[type='text']:hover {
+    background-color: #dcdee0;
+}
+
+input[type='text']:focus {
+    background-color: #ffffff;
+    border: 1px solid #1b1c1e;
+    color: #1b1c1e;
+}
 
         input::placeholder {
              font-size: 16px;
